07b44370a4dd34bc35078ab190a8b0533a26d5de
Marcin Dzikowski <dzikum@gmail.com>
2018-04-10 14:07:43 +0200

    added test checking

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -10,7 +10,8 @@ import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 import java.util.Date;
-
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
@@ -19,7 +20,8 @@ import static org.mockito.Mockito.when;
 public class BookKeeperTest {
 
     private BookKeeper bookKeeper;
-
+    private TaxPolicy mockedTaxPolicy = mock(TaxPolicy.class);
+    private Id id = new Id("123");
 
 
     @Test public void invoiceWithOnePosition() throws Exception {
@@ -44,4 +46,24 @@ public class BookKeeperTest {
         assertThat(invoiceResult.getItems().size(),is(1));
 
     }
+    @Test public void invoiceWithTwoPositionsShouldCallCalculateTaxMethodTwice() throws Exception{
+
+        ClientData client = new ClientData(id, "test");
+        InvoiceRequest invoiceRequest = new InvoiceRequest(client);
+
+        InvoiceFactory mockedInvoiceFactory = mock(InvoiceFactory.class);
+        bookKeeper = new BookKeeper(mockedInvoiceFactory);
+
+        ProductData product = new ProductData(id,Money.ZERO,"klawiatura",ProductType.STANDARD,new Date());
+        RequestItem item = new RequestItem(product,1,Money.ZERO);
+        invoiceRequest.add(item);
+        invoiceRequest.add(item);
+
+        when(mockedInvoiceFactory.create(client)).thenReturn(new Invoice(id,client));
+        when(mockedTaxPolicy.calculateTax(ProductType.STANDARD,Money.ZERO)).thenReturn(new Tax(Money.ZERO,"tax"));
+
+        Invoice invoiceResult = bookKeeper.issuance(invoiceRequest,mockedTaxPolicy);
+
+        verify(mockedTaxPolicy,times(2)).calculateTax(ProductType.STANDARD,Money.ZERO);
+}
 }
