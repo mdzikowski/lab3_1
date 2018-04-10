9d5d945003c4f364be6231fb473c3701edcaa715
Marcin Dzikowski <dzikum@gmail.com>
2018-04-10 15:05:37 +0200

    last fixes

--- src/main/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -1,6 +1,8 @@
 package pl.com.bottega.ecommerce.sales.application.api.handler;
-
+import org.hamcrest.generator.SugarGenerator;
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.internal.util.reflection.Whitebox;
@@ -10,43 +12,71 @@ import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
 import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
 import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
-
+import pl.com.bottega.ecommerce.sales.domain.client.Client;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import java.lang.reflect.Field;
 
 public class AddProductCommandHandlerTest {
-    AddProductCommandHandler commandHandler;
-    AddProductCommand productCommand;
-    Id id = new Id("123");
+    
 
-    @Mock
-    Reservation mockedReservationRepository;
-    AddProductCommandHandler addProductCommandHandler;
-    @Test public void methodSaveIsCalled() throws Exception {
+	private Id id = new Id("123");
+	
+	    private AddProductCommandHandler addProductCommandHandler;
+	    private ReservationRepository reservationRepository;
+	    private ProductRepository productRepository;
+	    private SuggestionService suggestionService;
 
-        commandHandler = new AddProductCommandHandler();
-        productCommand= new AddProductCommand(id,id,5);
+	    private Product product;
+	    private Reservation reservation;
 
-        ReservationRepository mockedReservation = mock(ReservationRepository.class);
-        ProductRepository mockedProduct = mock(ProductRepository.class);
+	    Field field;
+	    @Before public void setUp() {
+	    	        reservationRepository = mock(ReservationRepository.class);
+	    	        productRepository = mock(ProductRepository.class);
+	    	        suggestionService = mock(SuggestionService.class);
+	    	        product = mock(Product.class);
+	    	        reservation = mock(Reservation.class);
+       
 
-        Reservation reservation = new Reservation();
-        Product product = new Product();
+	    	        addProductCommandHandler = new AddProductCommandHandler();
 
-        //Dzialalo by na starszej wersji mockito :c
-        Whitebox.setInternalState(commandHandler,"reservationRepository",reservation);
+        
+	    	        try {
+	    	            field = AddProductCommandHandler.class.getDeclaredField("reservationRepository");
+	    	            field.setAccessible(true);
+	    	            field.set(addProductCommandHandler, reservationRepository);
 
-        when(mockedReservation.load(Mockito.any(Id.class))).thenReturn(reservation);
-        when(mockedProduct.load(Mockito.any(Id.class))).thenReturn(product);
+	    	            field = AddProductCommandHandler.class.getDeclaredField("productRepository");
+	    	            field.setAccessible(true);
+	    	            field.set(addProductCommandHandler, productRepository);
 
-        commandHandler.handle(productCommand);
+	    	            field = AddProductCommandHandler.class.getDeclaredField("suggestionService");
+	    	            field.setAccessible(true);
+	    	            field.set(addProductCommandHandler, suggestionService);
+
+	    	        } catch (Exception e) {
+	    	            e.printStackTrace();
+	    	        }
+	    	        when(reservationRepository.load(any(Id.class))).thenReturn(reservation);
+	    	        when(productRepository.load(any(Id.class))).thenReturn(product);
+	    	        when(suggestionService.suggestEquivalent(any(Product.class), any(Client.class))).thenReturn(product);
+
+	    	    }
+
+	    	    @Test public void methodSaveIsCalled() throws Exception {
+	    	        AddProductCommand addProductCommand = new AddProductCommand(id,id,1);
+	    	        when(product.isAvailable()).thenReturn(true);
+
+	    	        addProductCommandHandler.handle(addProductCommand);
+	    	        verify(reservationRepository,times(1)).save(any(Reservation.class));
+	    	    }
+        
 
 
-        verify(mockedReservation,times(1)).save(reservation);
-        //verify(commandHandler,times(1)).handle(productCommand);
-        //commandHandler.handle(productCommand);
+       
 
 
     }
 
-}
